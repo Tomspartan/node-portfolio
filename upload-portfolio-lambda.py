ed2c29cd94cf19d9ae71a7e2c2c55e4186b58aec
Tom Cummings <tomcummings@gmail.com>
2018-01-11 10:52:57 -0500

    add python upload script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('tomspartan.com')
+build_bucket = s3.Bucket('tomspartanbuildbucket')
+
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for name in myzip.namelist():
+       obj = myzip.open(name)
+       portfolio_bucket.upload_fileobj(obj, name,ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+       portfolio_bucket.Object(name).Acl().put(ACL='public-read')

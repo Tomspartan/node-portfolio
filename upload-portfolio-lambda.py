88cba2b0a72ca25cdbfc366149ea42b995a931f4
Tom Cummings <tomcummings@gmail.com>
2018-01-12 09:29:01 -0500

    updated lambda with python try/except catching

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,17 +5,24 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
-
-    portfolio_bucket = s3.Bucket('tomspartan.com')
-    build_bucket = s3.Bucket('tomspartanbuildbucket')
-
-
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-2:980720145618:deployPortfolioTopic')
+    try:
+        portfolio_bucket = s3.Bucket('tomspartan.com')
+        build_bucket = s3.Bucket('tomspartanbuildbucket')
 
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for name in myzip.namelist():
-           obj = myzip.open(name)
-           portfolio_bucket.upload_fileobj(obj, name,ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-           portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for name in myzip.namelist():
+               obj = myzip.open(name)
+               portfolio_bucket.upload_fileobj(obj, name,ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+               portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        print "Lambda job complete"
+        topic.publish(Subject="Portfolio deployed to S3 from github", Message='Portfolio deployed')
+    except:
+        topic.publish(Subject="Portfolio deploy failed", Message="Portfolio deploy failed")
+        raise
